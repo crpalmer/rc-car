2d5364e5cf8b7cdf667d8bb59a02504a276cf2a9
Christopher R. Palmer <crpalmer@gmail.com>
2022-04-15 15:32:56 -0400

    use the nes controller

--- rc-car.cpp
+++ rc-car.cpp
@@ -3,23 +3,31 @@
 #include "gp-input.h"
 #include "gp-output.h"
 #include "l298n.h"
+#include "nes.h"
+
+#define F_event "/dev/input/by-id/usb-0079_Controller-event-joystick"
 
 int
 main(int argc, char **argv)
 {
+    FILE *f;
+    nes_event_t e;
+
     gpioInitialise();
 
-    GPInput *b1 = new GPInput(23);
-    b1->set_pullup_up();
-    GPInput *b2 = new GPInput(24);
-    b2->set_pullup_up();
     motor_t *m = new L298N(new GPOutput(18), new GPOutput(15), new GPOutput(14));
 
-    while (1) {
-	if (b1->get_with_debounce()) {
+    if ((f = fopen(F_event, "r")) == NULL) {
+	perror(F_event);
+	exit(0);
+    }
+
+    while (nes_read(&e, f) >= 0) {
+	if (e.button != NES_UP_DOWN) {
+	} else if (e.dir > 0) {
 	    m->direction(1);
 	    m->speed(1);
-	} else if (b2->get_with_debounce()) {
+	} else if (e.dir < 0) {
 	    m->direction(0);
 	    m->speed(1);
 	} else {
